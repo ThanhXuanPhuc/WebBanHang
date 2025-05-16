caffeea06623bc4e7b31ade501a6f6042315e45b
ADMIN <ADMIN@PC346>
2025-05-16 11:29:17 +0700

    lab02 1

---
+++ WebBanHang/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebBanHang.Models;
+
+namespace WebBanHang.Controllers
+{
+    public class ProductController : Controller
+    {
+        private ApplicationDbContext _db;
+        public ProductController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            //Doc tat ca san pham trong CSDL 
+            var productList = _db.Products.Include(x => x.Category).ToList();
+            return View(productList);
+        }
+    }
+}
