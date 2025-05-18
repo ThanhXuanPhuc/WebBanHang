3549b31b1fea5ffb130ebf55232bcfabba3e1945
Thành Phúc <phucthanh.020405@gmail.com>
2025-05-18 21:06:51 +0700

    lab02 02

--- WebBanHang/Controllers/ProductController.cs
+++ WebBanHang/Controllers/ProductController.cs
@@ -1,7 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using WebBanHang.Models;
@@ -10,16 +14,142 @@ namespace WebBanHang.Controllers
 {
     public class ProductController : Controller
     {
-        private ApplicationDbContext _db;
-        public ProductController(ApplicationDbContext db)
+        private readonly ApplicationDbContext _db;
+        private readonly IWebHostEnvironment _hosting;
+        public ProductController(ApplicationDbContext db, IWebHostEnvironment hosting)
         {
             _db = db;
+            _hosting = hosting;
         }
         public IActionResult Index()
         {
-            //Doc tat ca san pham trong CSDL 
             var productList = _db.Products.Include(x => x.Category).ToList();
             return View(productList);
+        }
+        public IActionResult Add()
+        {
+            ViewBag.CategoryList = _db.Categories.Select(x => new SelectListItem
+            {
+                Value = x.Id.ToString(),
+                Text = x.Name
+            });
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Add(Product product, IFormFile ImageUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                if (ImageUrl != null)
+                {
+                    product.ImageUrl = SaveImage(ImageUrl);
+                }
+                _db.Products.Add(product);
+                _db.SaveChanges();
+                TempData["success"] = "Product inserted success";
+                return RedirectToAction("Index");
+            }
+            ViewBag.CategoryList = _db.Categories.Select(x => new SelectListItem
+            {
+                Value = x.Id.ToString(),
+                Text = x.Name
+            });
+            return View();
+        }
+        public IActionResult Update(int id)
+        {
+            var product = _db.Products.Find(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            ViewBag.CategoryList = _db.Categories.Select(x => new SelectListItem
+            {
+
+                Value = x.Id.ToString(),
+                Text = x.Name
+            });
+            return View(product);
+        }
+        [HttpPost]
+        public IActionResult Update(Product product, IFormFile ImageUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                var existingProduct = _db.Products.Find(product.Id);
+                if (ImageUrl != null)
+                {
+                    product.ImageUrl = SaveImage(ImageUrl);
+                    if (!string.IsNullOrEmpty(existingProduct.ImageUrl))
+                    {
+                        var oldFilePath = Path.Combine(_hosting.WebRootPath, existingProduct.ImageUrl);
+                        if (System.IO.File.Exists(oldFilePath))
+                        {
+                            System.IO.File.Delete(oldFilePath);
+                        }
+                    }
+                }
+                else
+                {
+                    product.ImageUrl = existingProduct.ImageUrl;
+                }
+                existingProduct.Name = product.Name;
+                existingProduct.Description = product.Description;
+                existingProduct.Price = product.Price;
+                existingProduct.CategoryId = product.CategoryId;
+                existingProduct.ImageUrl = product.ImageUrl;
+                _db.SaveChanges();
+                TempData["success"] = "Product updated success";
+                return RedirectToAction("Index");
+            }
+            ViewBag.CategoryList = _db.Categories.Select(x => new SelectListItem
+            {
+                Value = x.Id.ToString(),
+                Text = x.Name
+            });
+            return View();
+        }
+        private string SaveImage(IFormFile image)
+        {
+            var filename = Guid.NewGuid().ToString() + Path.GetExtension(image.FileName);
+            var path = Path.Combine(_hosting.WebRootPath, @"images/products");
+            var saveFile = Path.Combine(path, filename);
+            using (var filestream = new FileStream(saveFile, FileMode.Create))
+            {
+                image.CopyTo(filestream);
+            }
+            return @"images/products/" + filename;
+        }
+        public IActionResult Delete(int id)
+        {
+            var product = _db.Products.Find(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+        [HttpPost]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var product = _db.Products.Find(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            if (!String.IsNullOrEmpty(product.ImageUrl))
+            {
+                var oldFilePath = Path.Combine(_hosting.WebRootPath, product.ImageUrl);
+                if (System.IO.File.Exists(oldFilePath))
+                {
+                    System.IO.File.Delete(oldFilePath);
+                }
+            }
+            _db.Products.Remove(product);
+            _db.SaveChanges();
+            TempData["success"] = "Product deleted success";
+            //chuyen den action index
+            return RedirectToAction("Index");
         }
     }
 }
