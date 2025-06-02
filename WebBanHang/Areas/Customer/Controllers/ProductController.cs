1f14429f374fd7e61b44f2399cd1575cee80fdb2
ADMIN <ADMIN@PC346>
2025-06-02 10:27:32 +0700

    Customer 02

--- WebBanHang/Areas/Customer/Controllers/ProductController.cs
+++ WebBanHang/Areas/Customer/Controllers/ProductController.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebBanHang.Models;
-
+using WebBanHang.Areas.Customer.Models;
 namespace WebBanHang.Areas.Customer.Controllers
 {
     [Area("Customer")]
@@ -19,7 +19,11 @@ namespace WebBanHang.Areas.Customer.Controllers
         }
         public IActionResult Index(int catid = 1)
         {
-            var dsLoai = _db.Categories.OrderBy(x => x.DisplayOrder).ToList();
+            var dsLoai = _db.Categories.OrderBy(x => x.DisplayOrder)
+                .Select (c=> new CategoryViewModel { Id = c.Id, Name = c.Name,
+                                                    ToTalProduct = _db.Products.Where(p => p.CategoryId==c.Id).Count()})
+                .ToList();
+
             var dsSanPham = _db.Products.Where(p => p.CategoryId == catid).ToList();
             ViewBag.DSLOAI = dsLoai;
             return View(dsSanPham);
